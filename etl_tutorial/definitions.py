002b3e2b39ad478f14508fd3531d202598758f0a
Robert Kristof Paulsen <robert.paulsen@uib.no>
2025-03-11 13:29:18 +0100

    create downstream asset

--- etl_tutorial/definitions.py
+++ etl_tutorial/definitions.py
@@ -81,12 +81,60 @@ def sales_data(duckdb: DuckDBResource) -> dg.MaterializeResult:
                 "preview": dg.MetadataValue.md(preview_df.to_markdown(index=False))
             }
         )
+    
+@dg.asset(
+    compute_kind="duckdb",
+    group_name="joins",
+    deps=[
+        sales_data,
+        sales_reps,
+        products
+    ]
+)
+def joined_data(duckdb: DuckDBResource) -> dg.MaterializeResult:
+    with duckdb.get_connection() as conn:
+        conn.execute(
+            """
+            CREATE OR REPLACE VIEW joined_data AS (
+                SELECT
+                    date,
+                    dollar_amount,
+                    customer_name,
+                    quantity,
+                    rep_name,
+                    department,
+                    hire_date,
+                    product_name,
+                    category,
+                    price
+                FROM sales_data
+                LEFT JOIN sales_reps
+                    ON sales_reps.rep_id = sales_data.rep_id
+                LEFT JOIN products
+                    ON products.product_id = sales_data.product_id
+            )
+            """
+        )
+
+        preview_query = "SELECT * FROM joined_data LIMIT 10"
+        preview_df = conn.execute(preview_query).fetchdf()
+
+        row_count = conn.execute("SELECT COUNT(*) FROM joined_data").fetchone()
+        count = row_count[0] if row_count else 0
+
+        return dg.MaterializeResult(
+            metadata={
+                "row_count": dg.MetadataValue.int(count),
+                "preview": dg.MetadataValue.md(preview_df.to_markdown(index=False))
+            }
+        )
 
 defs = dg.Definitions(
     assets=[
         products,
         sales_reps,
-        sales_data
+        sales_data,
+        joined_data
     ],
     resources={
         "duckdb": DuckDBResource(database="data/mydb.duckdb")
